e523627a6231305ebf9f1ea2afecdc94fb5c2e69
ANDRE LUIZ ARANTES <andre_arantes@estudante.sesisenai.org.br>
2024-03-13 19:59:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+
+      <header></header>
+      <aside></aside>
+      <main></main>
+      <footer></footer>
+
+    </>
+
+  );
+
+}
+
+export default App
